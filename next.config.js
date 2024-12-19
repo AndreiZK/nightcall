df151f125b59fb5ad178319578b2f67d27ed033f
Jelmond <ant.soloviov@gmail.com>
2024-12-19 18:07:19 +0300

    feat: turn off ws

--- next.config.js
+++ next.config.js
@@ -21,18 +21,12 @@ const nextConfig = {
   images: {},
   webSocket: false,
   webpack(config, { isServer, dev }) {
-    // Disable HMR and WebSocket
     if (!isServer && dev) {
-      config.entry = async () => {
-        const entries = await (typeof config.entry === 'function' ? config.entry() : config.entry);
-        
-        // Remove HMR
-        if (entries['main.js'] && !entries['main.js'].includes('webpack-hot-middleware/client')) {
-          entries['main.js'] = entries['main.js'].filter(
-            (entry) => !entry.includes('webpack-hot-middleware/client')
-          );
-        }
-        return entries;
+      // Disable HMR
+      config.devServer = {
+        ...config.devServer,
+        hot: false,
+        liveReload: false
       };
     }
 
