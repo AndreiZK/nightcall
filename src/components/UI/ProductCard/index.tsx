c2c48629ce4b85bb57905011e7d0d6ba336dadd3
Jelmond <ant.soloviov@gmail.com>
2024-12-23 18:04:45 +0300

    feat: update images

--- src/components/UI/ProductCard/index.tsx
+++ src/components/UI/ProductCard/index.tsx
@@ -86,7 +86,8 @@ const ProductCard = (props: ProductCardProps) => {
         <>
             <StyledContainer onClick={() => setModalOpen(true)}>
                 <div className="image-container">
-                    <img src={BASE_IMAGE_URL + props.image[0]?.attributes?.url} alt="" />
+                    {/* @ts-expect-error */}
+                    <img src={BASE_IMAGE_URL + props.image[0].url} alt="" />
                 </div>
 
                 <div className="info">
