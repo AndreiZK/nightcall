57e9ccea6c56333f3f54428441aac59461ff1ce3
Jelmond <ant.soloviov@gmail.com>
2024-12-30 20:36:32 +0300

    feat: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -59,17 +59,17 @@ export default function App({ Component, pageProps }: AppProps) {
         html.style.setProperty("--font-noto", notoSans.style.fontFamily);
     });
 
-    // useEffect(() => {
-    // if (document.readyState === "complete") {
-    //     initializeTelegramWebApp();
-    //   } else {
-    //     window.addEventListener("load", initializeTelegramWebApp);
-    //   }
+    useEffect(() => {
+    if (document.readyState === "complete") {
+        initializeTelegramWebApp();
+      } else {
+        window.addEventListener("load", initializeTelegramWebApp);
+      }
   
-    //   return () => {
-    //     window.removeEventListener("load", initializeTelegramWebApp);
-    //   };
-    // }, []);
+      return () => {
+        window.removeEventListener("load", initializeTelegramWebApp);
+      };
+    }, []);
 
     return (
         <>
