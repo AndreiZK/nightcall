dc94ce36e67e809300c2ee280c7bb5c6ed314ea2
Jelmond <ant.soloviov@gmail.com>
2025-01-14 15:54:12 +0300

    feat; update tg pament

--- src/components/UI/Modal/OrderModal.tsx
+++ src/components/UI/Modal/OrderModal.tsx
@@ -192,20 +192,19 @@ const OrderModal = (props: Omit<ModalProps, "children">) => {
             jwt
         );
 
-        // if(!error){
-        //     let tg: any = window.Telegram.WebApp;
+        if(!error){
+            let tg: any = window.Telegram.WebApp;
 
-        //     const tgData = {
-        //       orderId: orderId,
-        //       paymentLink: paymentLink,
-        //       hashId: hashIds,
-        //     };
+            if(tg){
+                const tgData = {
+                    orderId: orderId,
+                    paymentLink: paymentLink,
+                    hashId: hashIds,
+                };
 
-        //     // useStore.setState({ paymentLink: paymentLink, hashId: hashIds });
-
-        //     tg.sendData(JSON.stringify(tgData));
-        // } else {
-        // }
+                tg.sendData(JSON.stringify(tgData));
+            }
+        }
 
         if(!error && paymentLink && hashIds){
             router.push(paymentLink);
