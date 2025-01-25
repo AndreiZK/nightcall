93dc88897e45c5c0bec5009fff48913230218f53
Jelmond <ant.soloviov@gmail.com>
2025-01-25 17:35:49 +0300

    feat

--- src/utils/cookieUtils.ts
+++ src/utils/cookieUtils.ts
@@ -7,7 +7,7 @@ const removeCookie = (name: string) => {
     const domain = window.location.hostname;
     document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${domain}; secure; samesite=strict`;
     // Also try without domain for local development
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
+    // document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
 };
 
 export { setCookie, removeCookie };
