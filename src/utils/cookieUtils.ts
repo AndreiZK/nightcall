a5ee4a165b493b53b94e286192c2bc9f51e051d4
Jelmond <ant.soloviov@gmail.com>
2025-01-25 17:38:11 +0300

    feat

--- src/utils/cookieUtils.ts
+++ src/utils/cookieUtils.ts
@@ -1,11 +1,13 @@
 const setCookie = (name: string, value: string, days: number = 1) => {
-    const domain = window.location.hostname;
-    document.cookie = `${name}=${value}; max-age=${days * 86400}; path=/; domain=${domain}; secure; samesite=strict`;
+    document.cookie = `${name}=${value};`;
+
+    console.log('settedCookies' ,document.cookie)
 };
 
 const removeCookie = (name: string) => {
-    const domain = window.location.hostname;
-    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${domain}; secure; samesite=strict`;
+    document.cookie = `${name}=;`;
+
+    console.log('removedCookies' ,document.cookie)
     // Also try without domain for local development
     // document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; samesite=strict`;
 };
