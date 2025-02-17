01d5a1376b6c82347d5677589f1ce8cc378c37e9
Jelmond <ant.soloviov@gmail.com>
2025-02-17 19:32:05 +0300

    feat: update strapi links

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
-// export const BASE_API_URL = "http://45.129.186.157:1337/";
-// export const BASE_IMAGE_URL = "http://45.129.186.157:1337";
+export const BASE_API_URL = "http://45.129.186.157:1337/";
+export const BASE_IMAGE_URL = "http://45.129.186.157:1337";
 // export const BASE_API_URL = "http://localhost:1337/";
 // export const BASE_IMAGE_URL = "http://localhost:1337";
-export const BASE_API_URL = "https://api.nightcall.by/";
-export const BASE_IMAGE_URL = "https://api.nightcall.by";
+// export const BASE_API_URL = "https://api.nightcall.by/";
+// export const BASE_IMAGE_URL = "https://aspi.nightcall.by";
 
 export const serviceId = "service_u64lr6h";
 export const templateId = "template_r6gjntm";
